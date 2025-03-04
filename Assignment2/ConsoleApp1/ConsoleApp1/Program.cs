23d636ca011fca7baf88a93aca86ecd4cdb7b3cb
Harshit Pathak <harshitpathak.hp567@gmail.com>
2025-03-04 07:32:32 +0530

    adding assignment 2

---
+++ Assignment2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ConsoleApp1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Logger log = GetLogger("file");
+        //log.Log();
+
+        ILoggerInterface logInterface = GetLogger2("file");
+        logInterface.Log();
+
+    }
+
+    private static Logger GetLogger(string type)
+    {
+        if (type == "db")
+        {
+            return new LogDb();
+        }
+        else if (type == "cloud")
+        {
+            return new LogCloud();
+        }
+
+        return new LogFile();
+    }
+    private static ILoggerInterface GetLogger2(string type)
+    {
+        if (type == "db")
+        {
+            return new LogDbInterface();
+        }
+        else if (type == "cloud")
+        {
+            return new LogCloudInterface();
+        }
+
+        return new LogFileInterface();
+    }
+}
